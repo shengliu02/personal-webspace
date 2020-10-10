5bb4f2896ef15ccf59a2c148131e6957cae0869c
Sheng Liu <sheng1302@gmail.com>
2020-10-10 07:43:54 -0400

    Updates

---
+++ precache-manifest.e72469d7f85fb907ffd0c46db674cd4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d157856d8e4731dddeddb77b0214c99",
+    "url": "/personal-webspace/index.html"
+  },
+  {
+    "revision": "b234809fef0c571791c2",
+    "url": "/personal-webspace/static/css/main.8dde67b1.chunk.css"
+  },
+  {
+    "revision": "f828f1bd7a050d599586",
+    "url": "/personal-webspace/static/js/2.0fdb38b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/personal-webspace/static/js/2.0fdb38b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b234809fef0c571791c2",
+    "url": "/personal-webspace/static/js/main.73e291a3.chunk.js"
+  },
+  {
+    "revision": "61cab1ba1f1e5c2de54d",
+    "url": "/personal-webspace/static/js/runtime-main.f856d005.js"
+  }
+]);
